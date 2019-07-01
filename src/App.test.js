35b1d55e529a4b591b2ffabbbf19d8ac12e3a297
Thomas Hands <scottcashreal@gmail.com>
2019-07-01 16:55:22 +0100

    Fixes failing test

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './configureStore';
 
@@ -8,7 +9,9 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
