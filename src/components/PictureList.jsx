986953159e654e0c68b9691f4c1a69f1dab6b5e3
Thomas Hands <scottcashreal@gmail.com>
2019-07-04 15:18:45 +0100

    update button width

--- src/components/PictureList.jsx
+++ src/components/PictureList.jsx
@@ -1,20 +1,17 @@
 import React from 'react';
 
-const PictureList = ({ pictureList, randomPicture, addToList }) => {
-
-    return (
-        <div className="random-picture-container">
-            <div className="picture-list-title" >Pictures List</div>
-            <div>
-                <button onClick={() => addToList(randomPicture)}>+ My Pictures</button>
-            </div>
-            <div className="picture-list-columns">
-                {pictureList.map((picture, index) => (
-                    <img key={index} alt="collection" src={picture} height="100" width="100" />
-                ))}
-            </div>
+const PictureList = ({ pictureList, randomPicture, addToList }) => (
+    <div className="random-picture-container">
+        <div className="picture-list-title" >Pictures List</div>
+        <div>
+            <button onClick={() => addToList(randomPicture)}>+ My Pictures</button>
         </div>
-    );
-};
+        <div className="picture-list-columns">
+            {pictureList.map((picture, index) => (
+                <img key={index} alt="collection" src={picture} height="100" width="100" />
+            ))}
+        </div>
+    </div>
+);
 
 export default PictureList;
